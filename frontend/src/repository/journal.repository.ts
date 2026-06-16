634e7068112e0c1acfb53e670136491e07a7c33a
Eduardo Alves Nunes <eduardo.nunes@koneksiagro.com.br>
2026-06-16 13:53:53 -0300

    feat: add missing crud in journal.repository

--- frontend/src/repository/journal.repository.ts
+++ frontend/src/repository/journal.repository.ts
@@ -1,6 +1,26 @@
 import Journal, { JournalType } from "@/models/Journal";
+import { Types } from "mongoose";
 
 export async function createJournal(data: JournalType) {
-
     return Journal.create(data);
+}
+
+export async function getJournalById(id: string) {
+    return Journal.findById(new Types.ObjectId(id));
+}
+
+export async function getAllJournals(limit: number = 10, skip: number = 0) {
+    return Journal.find().limit(limit).skip(skip).sort({ createdAt: -1 });
+}
+
+export async function updateJournal(id: string, data: Partial<JournalType>) {
+    return Journal.findByIdAndUpdate(
+        new Types.ObjectId(id),
+        data,
+        { new: true, runValidators: true }
+    );
+}
+
+export async function deleteJournal(id: string) {
+    return Journal.findByIdAndDelete(new Types.ObjectId(id));
 }
